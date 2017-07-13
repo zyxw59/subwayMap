8511cbe59ae22f5e907600686014bb84798f63a6
Emily Crandall Fleischman <emilycf@mit.edu>
2017-07-13 08:19:42 -0400

    Break out canvas functions to separate package

---
+++ canvas/canvas.go
@@ -0,0 +1,75 @@
+package canvas
+
+import (
+	"fmt"
+	"github.com/zyxw59/subwayMap/corner"
+	"io"
+)
+
+const (
+	header        = "<?xml version='1.0' encoding='utf-8' ?>"
+	stylesheetfmt = "<?xml-stylesheet type='text/css' href='%s.css'?>\n"
+	svginitfmt    = "<svg width=\"%v\" height=\"%v\"\n"
+	svgns         = "xmlns='http://www.w3.org/2000/svg'\nxmlns:xlink='http://www.w3.org/1999/xlink'>"
+	usefmt        = "<use xlink:href='#%s' class='%s' />\n"
+	whitebg       = "class='whitebg'"
+)
+
+type Canvas struct {
+	Writer io.Writer
+}
+
+func (c *Canvas) Print(a ...interface{}) (n int, err error) {
+	return fmt.Fprint(c.Writer, a...)
+}
+
+func (c *Canvas) Printf(format string, a ...interface{}) (n int, err error) {
+	return fmt.Fprintf(c.Writer, format, a...)
+}
+
+func (c *Canvas) Println(a ...interface{}) (n int, err error) {
+	return fmt.Fprintln(c.Writer, a...)
+}
+
+func (c *Canvas) PrintInit(width, height float64, stylesheet string) {
+	c.Println(header)
+	c.Printf(stylesheetfmt, stylesheet)
+	c.Printf(svginitfmt, width, height)
+	c.Println(svgns)
+}
+
+func (c *Canvas) PrintDefs(rsep float64, paths ...[]corner.Path) {
+	// begin line definitions
+	c.Println("<defs>")
+	for _, ps := range paths {
+		for _, p := range ps {
+			c.Println(p.Path(rsep))
+		}
+	}
+	// end line definitions
+	c.Println("</defs>")
+}
+
+func (c *Canvas) PrintPaths(paths ...[]corner.Path) {
+	for _, ps := range paths {
+		c.Println("<g>")
+		for _, p := range ps {
+			c.Printf(usefmt, p.Id, "whitebg")
+		}
+		for _, p := range ps {
+			c.Printf(usefmt, p.Id, p.Id)
+		}
+		c.Println("</g>")
+	}
+}
+
+func (c *Canvas) PrintClose() {
+	c.Println("</svg>")
+}
+
+func (c *Canvas) PrintAll(width, height float64, stylesheet string, rsep float64, paths ...[]corner.Path) {
+	c.PrintInit(width, height, stylesheet)
+	c.PrintDefs(rsep, paths...)
+	c.PrintPaths(paths...)
+	c.PrintClose()
+}
--- main.go
+++ main.go
@@ -1,75 +1,22 @@
 package main
 
 import (
-	"fmt"
+	"github.com/zyxw59/subwayMap/canvas"
 	"github.com/zyxw59/subwayMap/corner"
-	"io"
 	"os"
 )
-
-type Printer struct {
-	Writer io.Writer
-}
-
-func (p *Printer) Print(a ...interface{}) (n int, err error) {
-	return fmt.Fprint(p.Writer, a...)
-}
-
-func (p *Printer) Printf(format string, a ...interface{}) (n int, err error) {
-	return fmt.Fprintf(p.Writer, format, a...)
-}
-
-func (p *Printer) Println(a ...interface{}) (n int, err error) {
-	return fmt.Fprintln(p.Writer, a...)
-}
-
-func (canvas *Printer) PrintInit(width, height float64) {
-	canvas.Println(header)
-	canvas.Printf(stylesheetfmt, "lines")
-	canvas.Printf(svginitfmt, width, height)
-	canvas.Println(svgns)
-}
-
-func (canvas *Printer) PrintDefs(rsep float64, paths ...[]corner.Path) {
-	// begin line definitions
-	canvas.Println("<defs>")
-	for _, ps := range paths {
-		for _, p := range ps {
-			canvas.Println(p.Path(rsep))
-		}
-	}
-	// end line definitions
-	canvas.Println("</defs>")
-}
-
-func (canvas *Printer) PrintPaths(paths ...[]corner.Path) {
-	for _, ps := range paths {
-		canvas.Println("<g>")
-		for _, p := range ps {
-			canvas.Printf(usefmt, p.Id, "whitebg")
-		}
-		for _, p := range ps {
-			canvas.Printf(usefmt, p.Id, p.Id)
-		}
-		canvas.Println("</g>")
-	}
-}
-
-func (canvas *Printer) PrintClose() {
-	canvas.Println("</svg>")
-}
 
 func main() {
 	var (
 		paths               []corner.Path
 		paths2              []corner.Path
 		width, height, rsep float64
-		canvas              Printer
+		c                   canvas.Canvas
 	)
 	width = 2000
 	height = 2000
 	rsep = 10.0
-	canvas = Printer{os.Stdout}
+	c = canvas.Canvas{os.Stdout}
 	points := []corner.Point{{100, 100}, {100, 400}, {400, 400}, {100, 800}}
 	corners := corner.Sequence(points...)
 	p1 := corner.NewPath("a", corners, []int{1, 0, 0})
@@ -77,17 +24,5 @@ func main() {
 	p3 := corner.NewPath("c", corners, []int{0, 1, 1})
 	paths = append(paths, *p1, *p2)
 	paths2 = append(paths2, *p3)
-	canvas.PrintInit(width, height)
-	canvas.PrintDefs(rsep, paths, paths2)
-	canvas.PrintPaths(paths, paths2)
-	canvas.PrintClose()
+	c.PrintAll(width, height, "lines", rsep, paths, paths2)
 }
-
-const (
-	header        = "<?xml version='1.0' encoding='utf-8' ?>"
-	stylesheetfmt = "<?xml-stylesheet type='text/css' href='%s.css'?>\n"
-	svginitfmt    = "<svg width=\"%v\" height=\"%v\"\n"
-	svgns         = "xmlns='http://www.w3.org/2000/svg'\nxmlns:xlink='http://www.w3.org/1999/xlink'>"
-	usefmt        = "<use xlink:href='#%s' class='%s' />\n"
-	whitebg       = "class='whitebg'"
-)
